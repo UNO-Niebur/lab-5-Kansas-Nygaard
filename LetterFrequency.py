1b733e64af3cb022a8deea5b6a6a3ad432474382
Kansas-Nygaard <knygaard@unomaha.edu>
2026-02-23 06:01:44 +0000

    lab 5

--- LetterFrequency.py
+++ LetterFrequency.py
@@ -1,7 +1,7 @@
 #LetterFrequency.py
-#Name:
-#Date:
-#Assignment:
+#Name: Kansas Nyggard
+#Date: 02/22/26
+#Assignment: Lab 5
 
 #This program will create a CSV file of frequencies based on a text file.
 #Use Excel or similar spreadsheet software to visualize the frequencies of the CSV file.
